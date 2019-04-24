fff69238246d1bf413e7553264bdc4a07c0b0443
YUICHIROONUKI <yuichiro.o0209@gmail.com>
2019-04-24 18:41:29 +0900

    Valid

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth, if: :production?
+  before_action :basic_auth, if: :production?, except: :index
   protect_from_forgery with: :exception
 
   private
