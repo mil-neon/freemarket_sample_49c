5db6b0ca6587cdca2af3c3c967990f2e91eb5750
新村千里 <48665233+mil-neon@users.noreply.github.com>
2019-04-27 18:35:17 +0900

    購入確認画面作成 (#13)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
   root 'products#index'
+  resources :products, only: [:index]
+  resources :buyers, only: [:show]
   resources :products, only: [:index, :show]
 end
