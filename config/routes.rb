9b4e082da4a5cf08b9323d5bc5e8f38081b980a1
mil-neon <mil.www.25@icloud.com>
2019-04-28 15:56:18 +0900

    ビュー・コントローラ・ルートの作成＆諸所修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
   root 'products#index'
+  resources :users, only: [:new]
   resources :products, only: [:index]
   resources :buyers, only: [:show]
   resources :products, only: [:index, :show]
