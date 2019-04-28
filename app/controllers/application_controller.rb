491ad8fa7af43721faab8b9b661eb0c0821cf070
YUICHIROONUKI <yuichiro.o0209@gmail.com>
2019-04-28 13:54:46 +0900

    user_registration_page2
    ユーザーの会員情報入力ページ作りました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,18 +5,18 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :firstname_fullangle, :lastname_fullangle, :firstname_kana, :lastname_kana, :birthday, :postal_cord, :prefecture, :city, :address_number, :building_name, :phone_number, :icon_image, :profile])
   end
 
-  before_action :basic_auth, if: :production?, except: :index
-  protect_from_forgery with: :exception
+  # before_action :basic_auth, if: :production?, except: :index
+  # protect_from_forgery with: :exception
 
-  private
+  # private
 
-  def production?
-    Rails.env.production?
-  end
+  # def production?
+  #   Rails.env.production?
+  # end
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-    end
-  end
+  # def basic_auth
+  #   authenticate_or_request_with_http_basic do |username, password|
+  #     username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+  #   end
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
   root 'products#index'
   resources :products, only: [:index, :show]
+  resources :users, only: [:new]
 end
