1b9dac2a53589385cccc645cc746641b8dd2b166
mil-neon <mil.www.25@icloud.com>
2019-04-28 23:00:52 +0900

    提出用調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
   root 'products#index'
-  resources :users, only: [:new]
   resources :products, only: [:index]
   resources :buyers, only: [:show]
   resources :products, only: [:index, :show]
