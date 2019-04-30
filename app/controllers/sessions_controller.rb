54d71eac6008321992aacfdee2bd27c829697bfb
YUICHIROONUKI <yuichiro.o0209@gmail.com>
2019-04-30 18:24:23 +0900

    edit_routes.rb
    Rsourcesに書き直しました

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -26,7 +26,7 @@ class SessionsController < ApplicationController
     session[:firstname_kana] = params[:firstname_kana]
     session[:lastname_kana] = params[:lastname_kana]
     session[:birthday] = @birthday
-    redirect_to signup_phone_path
+    redirect_to signup_phone_registration_index_path
   end
 
   def create_address
@@ -36,12 +36,12 @@ class SessionsController < ApplicationController
     session[:address_number] = params[:address_number]
     session[:building_name] = params[:building_name]
     session[:phone_number] = params[:phone_number]
-    redirect_to signup_pay_path
+    redirect_to signup_pay_registration_index_path
   end
 
   def create
     @user = User.new(nickname: session[:nickname], email: session[:email], encrypted_password: session[:password], firstname_fullangle: session[:firstname_fullangle], lastname_fullangle: session[:lastname_fullangle], firstname_kana: session[:firstname_kana], lastname_kana: session[:lastname_kana], birthday: session[:birthday], postal_cord: session[:postal_cord], prefecture_id: session[:prefecture_id], city: session[:city], address_number: session[:address_number], building_name: session[:building_name], phone_number: session[:phone_number])
     @user.save
-    redirect_to complete_path
+    redirect_to complete_registration_index_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,14 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :new, :show]
   resources :users, only: [:new, :show]
   resources :buyers, only: [:show]
-  get 'registration' => 'sessions#new'
-  post 'create_user' => 'sessions#create_user'
-  get 'signup_phone' => 'sessions#signup_phone'
-  get 'signup_address' => 'sessions#signup_address'
-  post 'create_address' => 'sessions#create_address'
-  get 'signup_pay' => 'sessions#signup_pay'
-  post 'create' => 'sessions#create'
-  get 'complete' => 'sessions#complete'
+  resources :registration, controller: 'sessions', only: [:create_user, :signup_phone, :signup_address, :create_address, :signup_pay, :signup_pay, :complete, :create, :new] do
+    collection do
+      post 'create_user'
+      get 'signup_phone'
+      get 'signup_address'
+      post 'create_address'
+      get 'signup_pay'
+      get 'complete'
+    end
+  end
 end
