ea4adba45357255348fdc7b25f4481a9b6d06f3d
flooroolf <floor@floor-no-MacBook.local>
2019-04-30 20:14:14 +0900

    Edit category.rb
    ・親に当たるレコードは、parent_idを持たないため、nullを許容するために修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   has_many :products
   has_many :brands, through: :brands_categories
-  belongs_to :parent, class_name: :Category
+  belongs_to :parent, class_name: :Category, optional: true
   has_many :children, class_name: :Category, foreign_key: :parent_id, dependent: :destroy, inverse_of: :category
 end
