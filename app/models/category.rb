6e25f2d7cebc63aff434789494b4e56196dd8e4a
flooroolf <floor@floor-no-MacBook.local>
2019-04-30 19:39:44 +0900

    Edit category.rb
    ・inverse_of、dependent を設定

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ApplicationRecord
   has_many :products
   has_many :brands, through: :brands_categories
   belongs_to :parent, class_name: :Category
-  has_many :children, class_name: :Category, foreign_key: :parent_id
+  has_many :children, class_name: :Category, foreign_key: :parent_id, dependent: :destroy, inverse_of: :category
 end
