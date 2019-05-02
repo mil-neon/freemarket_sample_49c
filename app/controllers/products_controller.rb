19a97dba51a07e5536dcaac7f5770f863f1942db
flooroolf <floor@floor-no-MacBook.local>
2019-05-02 21:42:20 +0900

    Add Product new / create method
    ・商品出品機能を追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,11 +3,30 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
+    @product.images.build
+    @categories = Category.ransack(parent_id_null: true).result
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @categories = Category.ransack(parent_id_null: true).result
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def show
     @user = User.find(1)
     @products = Product.find(1)
     @images = @products.images.pluck("image")
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :category_id, :brand_id, :size, :condition, :shipping_feeh, :shipping_method, :prefecture_id, :shipping_date, :price).merge(seller_id: session[:user_id])
   end
 end
