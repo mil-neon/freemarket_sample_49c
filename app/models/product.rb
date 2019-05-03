dc5a90857d21198ef848036bdcad4669b0ac860f
flooroolf <floor@floor-no-MacBook.local>
2019-05-03 09:03:32 +0900

    Edit product.rb
    ・商品モデルにバリデーションを追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,18 @@
 class Product < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   has_many :likes, dependent: :destroy
   has_many :images, dependent: :destroy
   has_many :chats, dependent: :destroy
-  belongs_to :brand
+  belongs_to :brand, optional: true
   belongs_to :category
+
+  validates :name, :description, :category_id, :condition, :shipping_feeh, :shipping_method,:prefecture_id, :shipping_date, :price, :seller_id, presence: true
+  validates :name, length: { maximum: 40 }
+  validates :description, length: { maximum: 1000 }
+  validates :price,
+  numericality: {
+    greater_than_or_equal_to: 300,
+    less_than_or_equal_to: 9999999,
+    message: '300以上9999999以下で入力してください' 
+  }
 end
