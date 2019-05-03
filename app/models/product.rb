0e0397853806b4bb9f7b5f142a5754eec80acdc0
flooroolf <floor@floor-no-MacBook.local>
2019-05-03 09:28:53 +0900

    Edit product.rb by sider
    ・インデントを修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,9 +9,7 @@ class Product < ApplicationRecord
   validates :name, :description, :category_id, :condition, :shipping_feeh, :shipping_method, :prefecture_id, :shipping_date, :price, :seller_id, presence: true
   validates :name, length: { maximum: 40 }
   validates :description, length: { maximum: 1000 }
-  validates :price,
-  numericality: { greater_than_or_equal_to: 300,
-                  less_than_or_equal_to: 9_999_999,
-                  message: '300以上9999999以下で入力してください'
-  }
+  validates :price,numericality: { greater_than_or_equal_to: 300,
+                                   less_than_or_equal_to: 9_999_999,
+                                   message: '300以上9999999以下で入力してください' }
 end
