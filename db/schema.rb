0bd2fdd8c228720df92c4eeac9e200efcf0dfd18
YUICHIROONUKI <yuichiro.o0209@gmail.com>
2019-05-03 19:46:52 +0900

    add_asosiation
    外部キー制約、アソシエーションを追加

--- db/schema.rb
+++ db/schema.rb
@@ -45,11 +45,12 @@ ActiveRecord::Schema.define(version: 2019_05_03_064935) do
   end
 
   create_table "credits", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.string "customer_id", null: false
     t.string "card_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_credits_on_user_id"
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -113,6 +114,7 @@ ActiveRecord::Schema.define(version: 2019_05_03_064935) do
   add_foreign_key "brands_categories", "brands"
   add_foreign_key "brands_categories", "categories"
   add_foreign_key "categories", "categories", column: "parent_id"
+  add_foreign_key "credits", "users"
   add_foreign_key "images", "products"
   add_foreign_key "products", "brands"
   add_foreign_key "products", "categories"
