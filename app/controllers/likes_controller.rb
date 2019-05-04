3a9b734f1e8bc85df5a4981e45b560dce8f6dce2
RyunosukeFuku <rfutian64@gmail.com>
2019-05-04 11:03:53 +0900

    コントローラー記述変更

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,21 @@
 class LikesController < ApplicationController
+  before_action :set_variables
+
   def create
     @like = Like.new(user_id: session[:user_id], product_id: params[:product_id])
     @like.save
-    redirect_to("/products/#{params[:product_id]}")
+    redirect_to @product
   end
 
   def destroy
     @like = Like.find_by(user_id: session[:user_id], product_id: params[:product_id])
     @like.destroy
-    redirect_to("/products/#{params[:product_id]}")
+    redirect_to @product
+  end
+
+  private
+
+  def set_variables
+    @product = Product.find(params[:product_id])
   end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :user
-  belongs_to :product
+  belongs_to :product, counter_cache: :likes_count
 
   validates :product_id, presence: true
   validates :user_id, presence: true
