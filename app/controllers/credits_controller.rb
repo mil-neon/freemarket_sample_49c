e4d594c046c6a6b49a74eff920e35d56bd44f29c
YUICHIROONUKI <yuichiro.o0209@gmail.com>
2019-05-04 16:05:02 +0900

    credit_buy_function
    商品購入機能の実装

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -14,4 +14,15 @@ class CreditsController < ApplicationController
       end
     end
   end
+
+  def pay
+    card = Credit.where(user_id: session[:user_id]).first
+    if card.blank?
+      redirect_to new_signup_pay_path
+    else
+      Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+      Payjp::Charge.create(amount: 5000, customer: card.customer_id, currency: 'jpy')
+      redirect_to root_path
+    end
+  end
 end
