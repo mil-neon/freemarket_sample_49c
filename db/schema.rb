99cdcd14de463f04f063f5a42bfb30eccc5999d9
flooroolf <floor@floor-no-MacBook.local>
2019-05-06 18:28:07 +0900

    Add product model column
    ・商品モデルに、商品の出品状態を保持するカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_04_111757) do
+ActiveRecord::Schema.define(version: 2019_05_06_084459) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -26,14 +26,6 @@ ActiveRecord::Schema.define(version: 2019_05_04_111757) do
     t.datetime "updated_at", null: false
     t.index ["brand_id"], name: "index_brands_categories_on_brand_id"
     t.index ["category_id"], name: "index_brands_categories_on_category_id"
-  end
-
-  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "customer_id", null: false
-    t.string "card_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -78,6 +70,7 @@ ActiveRecord::Schema.define(version: 2019_05_04_111757) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "prefecture_id", null: false
+    t.integer "status", default: 1, null: false
     t.index ["brand_id"], name: "index_products_on_brand_id"
     t.index ["buyer_id"], name: "index_products_on_buyer_id"
     t.index ["category_id"], name: "index_products_on_category_id"
