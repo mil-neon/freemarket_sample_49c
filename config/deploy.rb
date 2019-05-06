fcea37e41588d595a1a6fe1d0b35836c3ec7872a
YUICHIROONUKI <yuichiro.o0209@gmail.com>
2019-05-06 21:18:19 +0900

    Deploy3

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,7 +38,7 @@ namespace :deploy do
       if test "[ ! -d #{shared_path}/config ]"
         execute "mkdir -p #{shared_path}/config"
       end
-      upload!('/credentials.yml.enc', "#{shared_path}/config/secrets.yml")
+      upload!('/config/credentials.yml.enc', "#{shared_path}/config/credentials.yml.enc")
     end
   end
   before :starting, 'deploy:upload'
