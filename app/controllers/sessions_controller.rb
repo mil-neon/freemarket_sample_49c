cc4a2054879204ab0f1f13f59565486dc124bc5c
YUICHIROONUKI <yuichiro.o0209@gmail.com>
2019-05-06 18:38:06 +0900

    Recaptcha

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,18 +10,22 @@ class SessionsController < ApplicationController
   def complete; end
 
   def create_user
-    @date = params["birthday(1i)"] + "-" + params["birthday(2i)"] + "-" + params["birthday(3i)"]
-    @birthday = @date.to_date
-    session[:nickname] = params[:nickname]
-    session[:email] = params[:email]
-    session[:password] = Rails.application.message_verifier('secret_key').generate(token: params[:password])
-    session[:password_confirmation] = params[:password_confirmation]
-    session[:firstname_fullangle] = params[:firstname_fullangle]
-    session[:lastname_fullangle] = params[:lastname_fullangle]
-    session[:firstname_kana] = params[:firstname_kana]
-    session[:lastname_kana] = params[:lastname_kana]
-    session[:birthday] = @birthday
-    redirect_to new_signup_phone_path
+    if verify_recaptcha
+      @date = params["birthday(1i)"] + "-" + params["birthday(2i)"] + "-" + params["birthday(3i)"]
+      @birthday = @date.to_date
+      session[:nickname] = params[:nickname]
+      session[:email] = params[:email]
+      session[:password] = Rails.application.message_verifier('secret_key').generate(token: params[:password])
+      session[:password_confirmation] = params[:password_confirmation]
+      session[:firstname_fullangle] = params[:firstname_fullangle]
+      session[:lastname_fullangle] = params[:lastname_fullangle]
+      session[:firstname_kana] = params[:firstname_kana]
+      session[:lastname_kana] = params[:lastname_kana]
+      session[:birthday] = @birthday
+      redirect_to new_signup_phone_path
+    else
+      redirect_to new_registration_path, notice: '※押してください'
+    end
   end
 
   def create_address
