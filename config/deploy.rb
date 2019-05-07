0abfe9e2cf6afff2b1ca6ad44cba922269c36147
YUICHIROONUKI <yuichiro.o0209@gmail.com>
2019-05-07 10:19:41 +0900

    basic_admin

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,6 +39,8 @@ set :default_env, {
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
   AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
   AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+  username: ENV["BASIC_AUTH_USER"]
+  password: ENV["BASIC_AUTH_PASSWORD"]
 }
 
 # Default branch is :master
