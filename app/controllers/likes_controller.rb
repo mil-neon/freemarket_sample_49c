b95836ec67b7609ac0aa94353f5375f767c1f037
RyunosukeFuku <rfutian64@gmail.com>
2019-05-13 17:39:36 +0900

    likesコントローラー記述

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,16 +1,14 @@
 class LikesController < ApplicationController
   before_action :set_variables
   def create
-    @like = Like.new(user_id: session[:user_id], product_id: params[:product_id])
-    @like.save
-    @likes_count = Product.find_by(id: params[:product_id])
-    redirect_to @product
+    @like = Like.create(user_id: session[:user_id], product_id: params[:product_id])
+    @product.reload
   end
 
   def destroy
     @like = Like.find_by(user_id: session[:user_id], product_id: params[:product_id])
     @like.destroy
-    redirect_to @product
+    @product.reload
   end
 
   def set_variables
