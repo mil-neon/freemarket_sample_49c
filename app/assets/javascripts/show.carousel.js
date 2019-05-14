db3c00a34d5b7a609b66f13dd513d10d313de3a4
新村千里 <48665233+mil-neon@users.noreply.github.com>
2019-05-14 18:18:21 +0900

    Scroll修正 (#71)

--- app/assets/javascripts/show.carousel.js
+++ app/assets/javascripts/show.carousel.js
@@ -9,4 +9,16 @@ $(document).on('turbolinks:load', function(){
   $( '.slick-main' ).on( 'mouseenter', '.slick-dots > li', function() {
     $( this ).click();
   });
+  $( '.slick-main' ).on( 'mouseover', '.slick-dots > li', function() {
+    $( this ).css({
+      opacity: "1",
+      cursor: "default"
+    });
+  });
+  $( '.slick-main' ).on( 'mouseout', '.slick-dots > li', function() {
+    $( this ).css({
+      opacity: "",
+      cursor: ""
+    });
+  });
 });
