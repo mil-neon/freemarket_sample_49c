58310fb23e22b89b2ead43dc0e99fb69f3ef3b4a
ankayuki <takahashiyuki@takahashiyukinoMacBook-puro.local>
2019-05-14 09:38:54 +0900

    Set user in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
+  before_action :set_user
   def new
   end
 
   def login
   end
-  
+
   def mypage
+  def set_user
+    @user = User.find(session[:user_id]) if session[:user_id] != nil
+  end
+
+  private
+
+  def update_params
+    params.permit(:nickname, :firstname_fullangle, :lastname_fullangle, :firstname_kana, :lastname_kana, :birthday, :postal_cord, :prefecture, :city, :address_number, :building_name, :phone_number, :icon_image, :profile)
   end
 end
